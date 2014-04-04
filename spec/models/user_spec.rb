fb83997c3514da63fb637c14336c63b0899a49d6
Hiroya Kabata <h.kabata420@gmail.com>
2014-04-04 16:34:27 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,6 @@ require 'spec_helper'
 describe User do
 
   before do
-
     @user = User.new(name: "Example User", email: "user@example.com", 
                      password: "foobar", password_confirmation: "foobar")
   end
@@ -27,8 +26,27 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-
+  it { should respond_to(:admin) }
+  
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(admin: true)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,8 +10,19 @@ def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+
+  cookies[:remember_token] = user.remember_token
 end
 
 def invalid_signin(user)
   click_button "Sign in"
+end
+
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",   with: user.email
+  fill_in "Password",with: user.password
+  click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
 end
