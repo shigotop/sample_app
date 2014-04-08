d0a4ecdfdcac0de6bdcb9c1d230b5bee911654a9
Hiroya Kabata <h.kabata420@gmail.com>
2014-04-08 15:20:13 +0900

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, 
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -20,6 +22,8 @@ class UsersController < ApplicationController
 
   def create
     if signed_in?
+      redirect_to root_path
+    else
       @user = User.new(params[:user])
       if @user.save
         sign_in @user
@@ -28,8 +32,6 @@ class UsersController < ApplicationController
       else
         render 'new'
       end
-    else
-      redirect_to root_path
     end
   end
 
@@ -52,12 +54,18 @@ class UsersController < ApplicationController
     redirect_to users_url
   end 
 
- 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in." 
-    end
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   def correct_user
